b47c64b212c2e13b1b4520dd8814259dcb4df904
barath keshav <62153832+mrmirror662@users.noreply.github.com>
2024-01-23 23:07:25 +0530

    Update schema.sql
    syntax error

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 create database if not exists mentor_management_system;
 
-use database mentor_management_system;
+use  mentor_management_system;
 
 create table student(name varchar(20) , usn char(10) primary key,password varchar(20),phone_no char(10),email varchar(20));
 
