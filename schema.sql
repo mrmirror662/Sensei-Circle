015d353fa458fcda7bd8004dc896d572b72314bd
barath keshav <62153832+mrmirror662@users.noreply.github.com>
2024-01-26 00:26:05 +0530

    Update schema.sql
    more character for email attribute

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ create database if not exists mentor_management_system;
 
 use  mentor_management_system;
 
-create table student(name varchar(20) , usn char(10) primary key,password varchar(20),phone_no char(10),email varchar(20));
+create table student(name varchar(20) , usn char(10) primary key,password varchar(20),phone_no char(10),email varchar(255));
 
