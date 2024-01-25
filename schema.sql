302b9bc14d641ae5d327698c474544c84ef6cec8
barath keshav <62153832+mrmirror662@users.noreply.github.com>
2024-01-26 00:29:07 +0530

    Update schema.sql
    added more chars to name and password

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ create database if not exists mentor_management_system;
 
 use  mentor_management_system;
 
-create table student(name varchar(20) , usn char(10) primary key,password varchar(20),phone_no char(10),email varchar(255));
+create table student(name varchar(255) , usn char(10) primary key,password varchar(32),phone_no char(10),email varchar(255));
 
