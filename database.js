f0952c3113cedd2522563f358595814d08594594
ajk-2409 <109851548+ajk-2409@users.noreply.github.com>
2024-02-06 22:38:05 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,99 +1,501 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-import mysql from 'mysql2/promise';
-import express from 'express'
-import bodyParser from 'body-parser';
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
+//config env var
+dotenv.config();
+
+//shortcut
+let log = console.log;
+
 // Create the connection to database
+const connection = await init();
+let deleteStudentIntervalId = setInterval(
+  deleteExpiredStudentSessions,
+  10 * 60 * 1000
+);
+deleteExpiredStudentSessions();
 
-//constants
-const PORT_NO = 3000;
-let app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+let deleteMentorIntervalId = setInterval(
+  deleteExpiredMentorSessions,
+  10 * 60 * 1000
+);
+deleteExpiredMentorSessions();
 
-// parse application/json
-app.use(bodyParser.json());
-// Get the client
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mentor_management_system',
-    password: 'Manwithoutlove@1024'
-});
-
-
-
-
-
-
-app.get('/', function (req, res) {
-    res.sendFile('index_test.html', { root: "." });
-
-});
-app.get('/login', function (req, res) {
-    res.sendFile('dummy_login.html', { root: "." });
-
-});
-app.post("/signup", async function (req, res) {
-    console.log(req.body);
-    const usn = req.body.usn;
-    const name = req.body.name;
-    const password = req.body.password;
-    const phone_no = req.body.phone_no;
-    const email = req.body.email;
-    
-    if(!usn || !name || !password || !phone_no || !email){
-        res.json({ flag: "404", msg: "No value entered in one of the fields" }); // end the response
-        return;
-    } 
-
-    let q_ = 'select usn from student where usn=?';
-    console.log(q_);
-    const [results1, fields1] = await connection.query(
-        q_,[usn]
+async function init() {
+  try {
+    let c = await mysql.createConnection({
+      host: process.env.host,
+      user: process.env.user,
+      database: process.env.db,
+    });
+    console.log(
+      `DB initialized with \nhost:${process.env.host}\nuser:${process.env.user}\ndb:${process.env.db}`
     );
-    console.log(results1.length);
-    if (results1.length !== 0) {
-        res.json({ flag: "404", msg: "account already exists" }); // end the response
-        return;
+    //TO:DO perform table checks;
+    return c;
+  } catch (err) {
+    console.error(`error db initialization Error:${err}`);
+    process.exit();
+  }
+}
+export async function StudentExists(usn) {
+  let q_ = "select usn from student_credentials where usn=?";
+
+  try {
+    const [results, fields] = await connection.query(q_, [usn]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw err;
+  }
+}
+
+export async function InsertStudent(student) {
+  let q = "insert into student_credentials values(?,?);";
+  let q_ = "insert into student_information values(?,?,?,?,?,?);";
+
+  try {
+    const [results, fields] = await connection.query(q, [
+      student.usn,
+      student.password,
+    ]);
+    const [results1, fields1] = await connection.query(q_, [
+      student.name,
+      student.usn,
+      student.phone_no,
+      student.email,
+      student.branch_id,
+      student.semester,
+    ]);
+  } catch (err) {
+    console.error("Error inserting user", err);
+    throw "Error inserting user";
+  }
+}
+export async function StudentCheckPassword(student) {
+  let q_ = "select usn,password from student_credentials where usn=?";
+
+  try {
+    const [results, fields] = await connection.query(q_, [student.usn]);
+
+    if (results.length == 0) {
+      return false;
     }
-    try {
-        let q = 'insert into student values(?,?,?,?,?)';
-        const [results, fields] = await connection.query(
-            q ,[name,usn,password,phone_no,email]
-        )
+    if (results[0]["password"] !== student.password) {
+      return false;
     }
-    catch (err) {
-        console.log(err);
-        res.json({ flag: "500", msg: "internal error" }); // end the response
-        res.end();
-        return;
+    return true;
+  } catch (err) {
+    console.error("Error authenticating  user", err);
+    throw "Error authenticating  user";
+  }
+}
+
+function generateSessionID() {
+  return Math.random().toString(36).substr(2, 8);
+}
+export async function AddStudentToSession(usn) {
+  let session_id = generateSessionID();
+  let q = "insert into student_session_info values(?,?,now());";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id, usn]);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+
+  return session_id;
+}
+export async function RemoveStudentSession(session_id) {
+  let q = "delete from student_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+}
+
+export async function IsStudentInSession(usn) {
+  let q = "select usn from student_session_info where usn=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [usn]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+export async function IsStudentInSessionSID(session_id) {
+  let q = "select session_id from student_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+
+export async function FetchUSNFromSID(session_id) {
+  let q = "select usn from student_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+    if (results.length == 0) throw "session not found";
+    return results[0]["usn"];
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+
+export async function deleteExpiredStudentSessions() {
+  console.log("deleting expired sessions");
+  let q =
+    "delete from student_session_info  where t < now() - interval 30 minute;";
+  try {
+    const [results, fields] = await connection.query(q);
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+}
+
+export async function MentorExists(mentor_id) {
+  let q_ = "select mentor_id from mentor_credentials where mentor_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q_, [mentor_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+
+export async function InsertMentor(mentor) {
+  let q = "insert into mentor_credentials values(?,?);";
+  let q_ = "insert into mentor_information values(?,?,?,?,?);";
+
+  try {
+    const [results, fields] = await connection.query(q, [
+      mentor.mentor_id,
+      mentor.password,
+    ]);
+    const [results1, fields1] = await connection.query(q_, [
+      mentor.name,
+      mentor.mentor_id,
+      mentor.phone_no,
+      mentor.email,
+      mentor.branch_id,
+    ]);
+  } catch (err) {
+    console.error("Error inserting user", err);
+    throw "Error inserting user";
+  }
+}
+export async function MentorCheckPassword(mentor) {
+  let q_ =
+    "select mentor_id,password from mentor_credentials where mentor_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q_, [mentor.mentor_id]);
+
+    if (results.length == 0) {
+      return false;
     }
-    res.json({ flag: "200", msg: "new accound signed up" });
-    res.end();
-});
-app.post("/login", async function (req, res) {
-    console.log(req.body);
-    const usn = req.body.usn;
-    const password = req.body.password;
-    let q = 'select * from student where usn=?';
-    console.log(q);
-    const [results, fields] = await connection.query(
-        q,[usn]
-    );
-    
-    if(!usn || results.length===0){
-        res.json({ flag: "404", msg: "incorrect usn" }); // end the response
-        return;
+    if (results[0]["password"] !== mentor.password) {
+      return false;
     }
-    console.log(results[0]["password"]);
-    if (results[0]["password"] != password) {
-        res.json({ flag: "404", msg: "incorrect password" }); // end the response
-        return;
-    }
-    res.json({ flag: "200", msg: "account found,logging in" });
-    res.end();
-});
-let server = app.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-});
+    return true;
+  } catch (err) {
+    console.error("Error authenticating  user", err);
+    throw "Error authenticating  user";
+  }
+}
+
+export async function AddMentorToSession(mentor_id) {
+  let session_id = generateSessionID();
+  let q = "insert into mentor_session_info values(?,?,now());";
+
+  try {
+    const [results, fields] = await connection.query(q, [
+      session_id,
+      mentor_id,
+    ]);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+
+  return session_id;
+}
+export async function RemoveMentorSession(session_id) {
+  let q = "delete from mentor_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+}
+
+export async function IsMentorInSession(mentor_id) {
+  let q = "select mentor_id from mentor_session_info where mentor_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+export async function IsMentorInSessionSID(session_id) {
+  let q = "select session_id from mentor_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+export async function FetchMentorIDFromSID(session_id) {
+  let q = "select mentor_id from mentor_session_info where session_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+    if (results.length == 0) throw "session not found";
+    return results[0]["mentor_id"];
+  } catch (err) {
+    console.error("Error checking user", err);
+    throw "Error checking user";
+  }
+}
+export async function deleteExpiredMentorSessions() {
+  console.log("deleting expired sessions");
+  let q =
+    "delete from mentor_session_info  where t < now() - interval 30 minute;";
+  try {
+    const [results, fields] = await connection.query(q);
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+}
+export async function CourseExists(course_id) {
+  let q_ = "select course_id from course_information where course_id=?";
+
+  try {
+    const [results, fields] = await connection.query(q_, [course_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking course", err);
+    throw "Error checking course";
+  }
+}
+
+export async function InsertCourse(course) {
+  let q = "insert into course_information values(?,?,?)";
+  try {
+    const [results, fields] = await connection.query(q, [
+      course.course_id,
+      course.course_name,
+      course.semester,
+    ]);
+  } catch (err) {
+    console.error("Error inserting course", err);
+    throw "Error inserting course";
+  }
+}
+
+export async function AcademiaExists(usn, course_id) {
+  let q =
+    "select usn,course_id from academic_details where usn=? and course_id=?";
+  try {
+    const [results, fields] = await connection.query(q, [usn, course_id]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking academia", err);
+    throw "Error checking academia";
+  }
+}
+
+export async function AcademiaInsert(academia) {
+  let q =
+    "INSERT INTO academic_details (usn, course_id, IA1, IA2, IA3, assignment_1, assignment_2, activity, Total_internal_marks,attendance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE IA1 = IFNULL(VALUES(IA1), IA1),IA2 = IFNULL(VALUES(IA2), IA2),IA3 = IFNULL(VALUES(IA3), IA3),assignment_1 = IFNULL(VALUES(assignment_1), assignment_1),assignment_2 = IFNULL(VALUES(assignment_2), assignment_2),activity = IFNULL(VALUES(activity), activity),Total_internal_marks = IFNULL(VALUES(Total_internal_marks), Total_internal_marks),attendance=IFNULL(VALUES(attendance),attendance);";
+  try {
+    const [results, fields] = await connection.query(q, [
+      academia.usn,
+      academia.course_id,
+      academia.ia_1,
+      academia.ia_2,
+      academia.ia_3,
+      academia.assignment_1,
+      academia.assignment_2,
+      academia.activity,
+      academia.total,
+      academia.attendance,
+    ]);
+  } catch (err) {
+    console.error("Error inserting academia", err);
+    throw "Error inserting academia";
+  }
+  let q_ =
+    "UPDATE academic_details SET Total_internal_marks= ifnull(IA1,0) + ifnull(IA2,0) + ifnull(IA3,0) + ifnull(assignment_1,0) +ifnull(assignment_2,0) + ifnull(activity,0) WHERE usn=? and course_id=?;";
+  try {
+    const [results, fields] = await connection.query(q_, [
+      academia.usn,
+      academia.course_id,
+    ]);
+  } catch (err) {
+    console.error("Error checking academia", err);
+    throw "Error checking academia";
+  }
+}
+
+export async function StudentAcademiaFetch(session_id) {
+  let q =
+    "select a.* from academic_details a , student_session_info s where s.session_id=? and a.usn=s.usn";
+  try {
+    const [results, fields] = await connection.query(q, [session_id]);
+    console.log(results);
+    return results;
+  } catch (err) {
+    console.error("Error checking academia", err);
+    throw "Error checking academia";
+  }
+}
+export async function MentorAcademiaFetch(session_id, usn) {
+  let q =
+    "select a.* from academic_details a , mentor_session_info m, student_mentor_table sm where m.session_id=? and m.mentor_id=sm.mentor_id and sm.usn=? and sm.usn=a.usn";
+  try {
+    const [results, fields] = await connection.query(q, [session_id, usn]);
+    console.log(results);
+    return results;
+  } catch (err) {
+    console.error("Error checking academia", err);
+    throw "Error checking academia";
+  }
+}
+export async function MentorStudentExists(mentor_id, usn) {
+  let q =
+    "select mentor_id,usn from student_mentor_table where mentor_id=? and usn=?";
+
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id, usn]);
+    if (results.length !== 0) return true;
+    return false;
+  } catch (err) {
+    console.error("Error checking mentor_student", err);
+    throw "Error checking mentor_student";
+  }
+}
+export async function RegisterStudentToMentor(mentor_id, usn) {
+  let q = "insert into student_mentor_table values(?,?)";
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id, usn]);
+  } catch (err) {
+    console.error("Error registering to mentor", err);
+    throw "Error registering student to mentor";
+  }
+}
+
+export async function FetchStudentDetailsFromMentorID(mentor_id) {
+  let q =
+    "select si.* from student_mentor_table sm, student_information si where sm.mentor_id = ? and sm.usn = si.usn;";
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id]);
+    return results;
+  } catch (err) {
+    console.error("Error fetching student details ", err);
+    throw "Error fetching student details ";
+  }
+}
+
+export async function MentorPushNotification(mentor_id, msg) {
+  let q = "insert into notification values(?,now(),?);";
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id, msg]);
+  } catch (err) {
+    console.error("Error inserting notification", err);
+    throw "Error inserting notification";
+  }
+}
+
+export async function MentorFetchNotification(mentor_id) {
+  let q =
+    "select * from notification where mentor_id = ? order by time desc limit 30;";
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id]);
+    return results;
+  } catch (err) {
+    console.error("Error fetching notification", err);
+    throw "Error fetching notification";
+  }
+}
+
+export async function StudentFetchNotification(usn) {
+  let q =
+    "select n.time , n.msg from notification n,student_mentor_table ms where ms.usn = ? and ms.mentor_id = n.mentor_id ";
+  try {
+    const [results, fields] = await connection.query(q, [usn]);
+    return results;
+  } catch (err) {
+    console.error("Error fetching notification", err);
+    throw "Error fetching notification";
+  }
+}
+
+export async function StudentPushIssue(usn, msg) {
+  let q = "insert into issues values(?,now(),?);";
+  try {
+    const [results, fields] = await connection.query(q, [usn, msg]);
+  } catch (err) {
+    console.error("Error inserting issues", err);
+    throw "Error inserting issues";
+  }
+}
+
+export async function MentorFetchIssue(mentor_id) {
+  let q =
+    "select i.time , i.msg from issues i where i.usn in(select usn from student_mentor_table sm where sm.mentor_id=?) order by time desc limit 30;"; //TO:FIX (bs code)
+  try {
+    const [results, fields] = await connection.query(q, [mentor_id]);
+    return results;
+  } catch (err) {
+    console.error("Error fetching issues", err);
+    throw "Error fetching issues";
+  }
+}
+
+export async function StudentFetchIssue(usn) {
+  let q = "select * from issues where usn = ? order by time desc limit 30";
+  try {
+    const [results, fields] = await connection.query(q, [usn]);
+    return results;
+  } catch (err) {
+    console.error("Error fetching issues", err);
+    throw "Error fetching issues";
+  }
+}
--- schema.sql
+++ schema.sql
@@ -2,5 +2,26 @@ create database if not exists mentor_management_system;
 
 use  mentor_management_system;
 
-create table student(name varchar(20) , usn char(10) primary key,password varchar(20),phone_no char(10),email varchar(20));
+create table if not exists student_credentials( usn char(10) primary key,password varchar(32));
 
+create table if not exists branch(branch_id varchar(10) primary key,branch_name varchar(32));
+
+create table if not exists student_information(name varchar(255) , usn char(10) primary key,phone_no char(10),email varchar(255),branch_id char(10),semester int, foreign key(usn) references student_credentials(usn) on delete cascade );
+
+create table if not exists course_information(course_id char(10) primary key,course_name varchar(255),course_credits int,semester int);
+
+create table if not exists academic_details(usn char(10) ,course_id char(10),IA1 int,IA2 int,IA3 int,assignment_1 int,assignment_2 int,activity int,Total_internal_marks int,attendance int, primary key(usn,course_id),foreign key (usn) references student_information(usn),foreign key (course_id) references course_information(course_id));
+
+create table if not exists mentor_credentials( mentor_id char(10) primary key,password varchar(32));
+
+create table if not exists mentor_information(name varchar(255) , mentor_id char(10) primary key,phone_no char(10),email varchar(255),branch_id char(10), foreign key(mentor_id) references mentor_credentials(mentor_id) on delete cascade );
+
+create table if not exists student_session_info(session_id char(8) , usn char(10), t timestamp);
+
+create table if not exists mentor_session_info(session_id char(8) , mentor_id char(10), t timestamp);
+
+create table if not exists student_mentor_table(mentor_id char(10),usn char(10),primary key (mentor_id,usn),foreign key(mentor_id) references mentor_credentials(mentor_id) on delete cascade,foreign key(usn) references student_credentials(usn) on delete cascade );
+
+create table if not exists notification(mentor_id char(10),time timestamp,msg text, primary key(mentor_id,time));
+
+create table if not exists issues(usn char(10),time timestamp,msg text, primary key(usn,time));
